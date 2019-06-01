5e40828c75abefc73117cd9abc56073dfc6db47b
Alexandr <zubkov.alexander.v@gmail.com>
2019-06-01 20:56:34 +0400

    Added javadoc for class-material

--- src/model/material/Material.java
+++ src/model/material/Material.java
@@ -6,39 +6,82 @@ import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
+/**
+ * Материал. Узел дерева материалов.
+ */
 public class Material {
 
+    /**
+     *  Название материала
+     */
     private String _name;
-    private List<Material> _heirs = new ArrayList<>();
+    /**
+     *  Потомки текущего материала
+     */
+    private List<Material> _heirs;
+    /**
+     *  Родитель текущего материала
+     */
     private Material _parent = null;
 
+    /**
+     *  Изображение, которое описывает текущий материал
+     */
     private BufferedImage _image = null;
 
+    /**
+     * @param name название материала
+     * @param parent ссылка на родительский материал
+     */
     Material(String name, Material parent) {
+
+        _heirs = new ArrayList<>();
+
         this._name = name;
+
         if (parent != null){
             this._parent = parent;
             parent.set_heirs(this);
         }
+
     }
 
+    /**
+     * @param _heirs ссылка на дочерний материал
+     */
     private void set_heirs(Material _heirs) {
         this._heirs.add(_heirs);
     }
 
-    void set_image(String path, String name) throws IOException {
+    /**
+     * @param path путь ко всем изображением
+     * @param name название изображения (без маски)
+     * @throws IOException нет такого изображения
+     */
+    private void set_image(String path, String name) throws IOException {
         this._image = ImageIO.read(new File(path, name + ".png"));
     }
 
+    /**
+     * @return изображение, которое описывает текущий материал
+     */
     public BufferedImage get_image() {
         return _image;
     }
 
+    /**
+     * @param Other сравниваемый материал
+     * @return true - если материалы совместимы, иначе false
+     */
     public boolean connectability(Material Other){
         return compareParents(Other) || compareHeirs(Other);
     }
 
-    public boolean  compareParents(Material material){
+    /**
+     * @param material сравнивыемый материал
+     * @return true - если материал является родительским, иначе false
+     */
+    private boolean  compareParents(Material material){
         if (this.equals(material))
             return true;
         if (_parent!=null)
@@ -46,7 +89,11 @@ public class Material {
         return false;
     }
 
-    public boolean compareHeirs(Material material){
+    /**
+     * @param material сравнивыемый материал
+     * @return true - если материал является дочерним, иначе false
+     */
+    private boolean compareHeirs(Material material){
         if (this.equals(material))
             return true;
         for(Material heir: _heirs){
@@ -55,6 +102,11 @@ public class Material {
         }
         return false;
     }
+
+    /**
+     * @param o сравнивыемый материал
+     * @return true - если материал является таким же, иначе false
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -65,11 +117,17 @@ public class Material {
                 _parent.equals(material._parent);
     }
 
+    /**
+     * @return hash объекта
+     */
     @Override
     public int hashCode() {
         return Objects.hash(_name, _heirs, _parent, _image);
     }
 
+    /**
+     * @return название материала
+     */
     @Override
     public String toString() {
         return  _name ;
