39ce0a2cb8c135f1ee27fb967371f98744069445
Alexandr <zubkov.alexander.v@gmail.com>
2019-06-01 21:14:26 +0400

    Added javadoc for class-configuration

--- src/model/ConfigurationGame.java
+++ src/model/ConfigurationGame.java
@@ -1,6 +1,15 @@
 package model;
 
+/**
+ * Конфигурационный класс для игры
+ */
 public class ConfigurationGame {
+    /**
+     * Путь к изображениям
+     */
     public static String path = System.getProperty("user.dir") + "\\resources\\Image";
+    /**
+     * Путь к файлам описания уровней
+     */
     public static String pathLvl = System.getProperty("user.dir") + "\\resources";
 }
