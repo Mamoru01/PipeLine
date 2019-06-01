4794f6033662461613323805f08efa910670ffaf
Alexandr <zubkov.alexander.v@gmail.com>
2019-06-01 21:12:10 +0400

    Added javadoc for interface events

--- src/model/events/UnitPipeActionListner.java
+++ src/model/events/UnitPipeActionListner.java
@@ -2,7 +2,19 @@ package model.events;
 
 import java.util.EventListener;
 
+/**
+ * Интервейс-слушателей события сегмента трубы
+ */
 public interface UnitPipeActionListner extends EventListener {
+    /**
+     * Провести воду
+     */
     void conductWater();
+
+    /**
+     * Разлить воду
+     */
     void pourWater();
+
+    //TODO Создать один метод с параметром
 }
--- src/model/events/ViewActionListner.java
+++ src/model/events/ViewActionListner.java
@@ -2,6 +2,12 @@ package model.events;
 
 import java.util.EventListener;
 
+/**
+ * Интервейс-слушателей события обновления view
+ */
 public interface ViewActionListner extends EventListener {
+    /**
+     * Обновить view
+     */
     void updateView();
 }
